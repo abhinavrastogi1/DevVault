96ae35b9ce2730678d7ef86bd471d9e4152a102d
Abhinav Rastogi <abhinavrastogi986@gmail.com>
2025-06-12 17:21:33 +0530

    added favicon

--- Backend_DevVault/src/server.js
+++ Backend_DevVault/src/server.js
@@ -8,7 +8,7 @@ const server = async () => {
   try {
     await connect_DB();
     app.get("/", (req,res) => {
-      res.redirect(302,"https://clikn.in")
+      res.redirect(302,"https://devvault.clikn.in")
     });
     app.listen(port, "0.0.0.0", () => {
       console.log(`Server is running on port ${port}`);
